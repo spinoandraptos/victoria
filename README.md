f2b42bd902da030bbd901b4216e7bd3bf2a2b45e
spinoandraptos <spinoandraptos@gmail.com>
2026-08-28 16:02:41 +0800

    Update: Revive opx 28082026

--- README.md
+++
@@ -1,2 +0,0 @@
-# project-template
-Template project repository with some standard measurement and characterization scripts. To start a new project, create a new fork of this project.
--- config/test.py
+++
@@ -1,9 +0,0 @@
-from Pyro5.api import Proxy
-
-opx = Proxy("PYRO:opx1000@localhost:9090")
-
-# Force the connection and binding immediately
-opx._pyroBind() 
-
-# Now check the representation
-print(f"INSTRUMENTS: { {'opx1000': opx} }")
